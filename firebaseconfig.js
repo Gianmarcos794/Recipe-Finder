d9bdda84457890ce0572859a5589b59fd4ed22b0
Gianmarcos794 <129087020+Gianmarcos794@users.noreply.github.com>
2025-04-21 16:24:36 -0400

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2F92UsRhwMTpbBjyjVBp_yNJSX2TWct8",
+  authDomain: "recipe-finder-f8d1d.firebaseapp.com",
+  projectId: "recipe-finder-f8d1d",
+  storageBucket: "recipe-finder-f8d1d.firebasestorage.app",
+  messagingSenderId: "595883546777",
+  appId: "1:595883546777:web:862fb76913440a44f38008",
+  measurementId: "G-BPFWH5SMWD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
